6548473a7e5081e0344d658374be14489ce533bc
Sergio Alonso Benito <ser_al_97@outlook.com>
2021-02-23 16:37:47 +0100

    Mejora de test_Post

--- tests/test_Post.py
+++ tests/test_Post.py
@@ -1,4 +1,3 @@
-from flask import jsonify
 import json
 
 
@@ -6,3 +5,6 @@ def test_Post(app, client):
     data = {"surname": "Tio", "lastname": "Guay", "name": "Charlie"}
     res = client.post('/posts', data=json.dumps(data), headers={"Content-Type": "application/json"})
     assert res.status_code == 200
+    assert res.json['name'] == data['name']
+    assert res.json['surname'] == data['surname']
+    assert res.json['lastname'] == data['lastname']
