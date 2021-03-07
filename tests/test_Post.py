ffd3c0906a826d1c4b4cc952d0ff03759da03638
Luis Nicolas-Alonso <luisfernicolas@gmail.com>
2021-03-07 12:48:31 +0100

    Fix tests

--- tests/test_Post.py
+++ tests/test_Post.py
@@ -2,7 +2,7 @@ import json
 from unittest import mock
 
 
-def test_post(client):
+def test_post_success(client, mock_github_response):
     input_data = {
         "surname": "Mola",
         "lastname": "Mazo",
@@ -18,15 +18,7 @@ def test_post(client):
         "github_following": 1,
     }
 
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-                return self.json_data
-
-    response = MockResponse([{"login": "a"}], 200)
+    response = mock_github_response([{"login": "a"}], 200)
     with mock.patch("main.requests.get", return_value=response):
         res = client.post(
             "/posts",
@@ -41,3 +33,33 @@ def test_post(client):
     assert response_json["surname"] == expected_response["surname"]
     assert response_json["lastname"] == expected_response["lastname"]
     assert response_json["github_following"] == expected_response["github_following"]
+
+
+
+def test_post_error(client, mock_github_response):
+    input_data = {
+        "surname": "Mola",
+        "lastname": "Mazo",
+        "name": "Charlie",
+        "github_name": "SERGIOALOB",
+    }
+
+    expected_response = {
+        "surname": "Mola",
+        "lastname": "Mazo",
+        "name": "Charlie",
+        "github_name": "SERGIOALOB",
+        "github_following": 1,
+    }
+
+    response = mock_github_response([{"error": "not found"}], 400)
+    with mock.patch("main.requests.get", return_value=response):
+        res = client.post(
+            "/posts",
+            json=input_data,
+            headers={"Content-Type": "application/json"},
+        )
+
+    response_json = res.json
+    assert res.status_code == 400
+    assert response_json == {'message': 'Github user not found'}
